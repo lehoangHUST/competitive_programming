394223ef99e2d4d08c7a5bf0473f9e6454b4e66b
lehoangHUST <lehoangEDABK@gmail.com>
2024-12-23 01:36:28 +0700

    [Update]: Add solve optimize for problems 396

--- leetcode/Medium/396_rotate_function.py
+++ leetcode/Medium/396_rotate_function.py
@@ -1,18 +1,18 @@
 class Solution:
     def maxRotateFunction(self, nums: List[int]) -> int:
-        
-        if len(nums) == 1:
-            return 0
-        maxValue = 0
-        for idx, num in enumerate(nums):
-            maxValue += idx * num
-        
         n = len(nums)
+        if n == 1:
+            return 0
+        
+        # Tính tổng ban đầu của F(0) và tổng tất cả các phần tử trong nums
         total = sum(nums)
-        F = maxValue
-        # consider value
+        F = sum(i * num for i, num in enumerate(nums))
+        
+        maxValue = F
+        
+        # Dùng công thức để tính F(k) từ F(k-1)
         for idx in range(1, n):
-            F += total - n * nums[n - idx]
+            F += total - n * nums[-idx]
             maxValue = max(maxValue, F)
         
         return maxValue
