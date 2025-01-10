a3304d71cb78eaca41cf530436b027368b4b43fe
lehoangHUST <lehoangEDABK@gmail.com>
2025-01-11 01:07:55 +0700

    [Update]: Add optimize solve for problem 64

--- leetcode/Medium/64_Minimum_Path_Sum.py
+++ leetcode/Medium/64_Minimum_Path_Sum.py
@@ -2,17 +2,16 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        min_matrix = 0
-        for row in range(rows):
-            for col in range(cols):
-                min_matrix += grid[row][col]
-                if row == 0 and col != 0:
-                    grid[row][col] += grid[row][col - 1]
-                if col == 0 and row != 0:
-                    grid[row][col] += grid[row - 1][col]
-        # calculate sum
+        
+        # Khởi tạo hàng đầu tiên và cột đầu tiên
+        for row in range(1, rows):
+            grid[row][0] += grid[row - 1][0]  # Tính tổng dọc cột đầu tiên
+        for col in range(1, cols):
+            grid[0][col] += grid[0][col - 1]  # Tính tổng ngang hàng đầu tiên
+        
+        # Tính toán các giá trị tối thiểu cho phần còn lại của ma trận
         for row in range(1, rows):
             for col in range(1, cols):
-                min_matrix = min(grid[row - 1][col] + grid[row][col], grid[row][col - 1] + grid[row][col]) 
-                grid[row][col] = min_matrix
-        return min_matrix
+                grid[row][col] += min(grid[row - 1][col], grid[row][col - 1])
+        
+        return grid[-1][-1]
